3edcbe3ce5b696b755ac123568719fcba364f3f9
sakitamako <sa.919.1025.ki@gmail.com>
2024-03-27 20:35:55 +0900

    編集した

--- Rensyu2/src/Rensyu2/sakitamako/com/github/ListSample02.java
+++ Rensyu2/src/Rensyu2/sakitamako/com/github/ListSample02.java
@@ -8,7 +8,7 @@ public class ListSample02 {
 	public static void main(String[] args) {
 	        List<String> list = new ArrayList<String>();
 	        
-	        //値の記憶は　add メソッドを利用
+	        //値の記憶は　add メソッドを利用します
 	        list.add("1");
 	        list.add("2");
 	        list.add("3");
@@ -17,11 +17,11 @@ public class ListSample02 {
 	        
 	        //値の取得
 	        for (int i = 0; i < list.size(); i++) {
-	            //値の取得は get メソッドを利用
+	            //値の取得は get メソッドを利用します
 	            System.out.println(list.get(i));
 	        }
 
-	        //拡張 for 文を利用するともっと簡単
+	        //拡張 for 文を利用するともっと簡単です
 	        for (String s : list) {
 	            System.out.println(s); 
 	    }
